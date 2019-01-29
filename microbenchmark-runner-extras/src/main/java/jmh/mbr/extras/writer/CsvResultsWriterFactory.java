d2baae01fbd792c659bb21e6b0c94838c9f491f1
Mark Paluch <mpaluch@pivotal.io>
2019-01-29 08:57:29 +0100

    Polishing
    Create microbenchmark-runner-extras module. Move CSV writer to jmh.mbr.extras.writer. Change license headers to EPL. Extract CsvResultsWriter into class and extract methods. Adjust locale-specific bits in tests to reuse same locale settings.
    
    Original pull request: #21.

---
+++ microbenchmark-runner-extras/src/main/java/jmh/mbr/extras/writer/CsvResultsWriterFactory.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+package jmh.mbr.extras.writer;
+
+import jmh.mbr.core.ResultsWriter;
+import jmh.mbr.core.ResultsWriterFactory;
+
+/**
+ * A {@link ResultsWriterFactory} that writes the output in csv format (to the console and to a file). Activated with
+ * <code>-DpublishTo=csv:./path/to/file.csv</code>. The file will be overwritten if it already exists. If the file
+ * cannot be written a warning will be printed on the console.
+ */
+public class CsvResultsWriterFactory implements ResultsWriterFactory {
+
+	@Override
+	public ResultsWriter forUri(String uri) {
+
+		if (!uri.startsWith("csv:")) {
+			return null;
+		}
+
+		return new CsvResultsWriter(uri);
+	}
+}
--- microbenchmark-runner-extras/src/test/java/jmh/mbr/extras/writer/CsvResultsWriterFactoryTests.java
+++ microbenchmark-runner-extras/src/test/java/jmh/mbr/extras/writer/CsvResultsWriterFactoryTests.java
@@ -1,19 +1,15 @@
 /*
- * Copyright 2016-2017 the original author or authors.
+ * Copyright 2019 the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * http://www.eclipse.org/legal/epl-v20.html
  */
-package jmh.mbr.core.writer;
+package jmh.mbr.extras.writer;
+
+import static org.assertj.core.api.Assertions.*;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -38,20 +34,16 @@ import org.openjdk.jmh.runner.format.OutputFormat;
 import org.openjdk.jmh.runner.format.OutputFormatFactory;
 import org.openjdk.jmh.runner.options.TimeValue;
 import org.openjdk.jmh.runner.options.VerboseMode;
+import org.openjdk.jmh.util.ScoreFormatter;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-/**
- * @author Dave Syer
- */
-public class CsvResultsWriterFactoryTests {
+class CsvResultsWriterFactoryTests {
 
 	private static String TARGET_FILE = "target/result.csv";
 
 	private CsvResultsWriterFactory factory = new CsvResultsWriterFactory();
 
 	@BeforeEach
-	public void init() {
+	void init() {
 		File target = new File(TARGET_FILE);
 		if (target.exists()) {
 			assertThat(target.delete()).isTrue();
@@ -75,7 +67,7 @@ public class CsvResultsWriterFactoryTests {
 	@Test
 	void writeActualResults() {
 		BenchmarkParams params = params();
-		Collection<BenchmarkResult> data = Arrays.<BenchmarkResult> asList(new BenchmarkResult(null, data()));
+		Collection<BenchmarkResult> data = Collections.singletonList(new BenchmarkResult(null, data()));
 		RunResult runResult = new RunResult(params, data);
 		String result = output(runResult);
 		assertThat(result).containsSubsequence("class, method, median, mean, range", "Foo, exec");
@@ -84,40 +76,35 @@ public class CsvResultsWriterFactoryTests {
 	@Test
 	void writeParamResult() {
 		BenchmarkParams params = params("a=b");
-		Collection<BenchmarkResult> data = Arrays.<BenchmarkResult> asList(new BenchmarkResult(null, data()));
+		Collection<BenchmarkResult> data = Collections.singletonList(new BenchmarkResult(null, data()));
 		RunResult runResult = new RunResult(params, data);
 		String result = output(runResult);
-		// System.err.println(result);
 		assertThat(result).containsSubsequence("class, method, a, median, mean, range", "Foo, exec, b");
 	}
 
 	@Test
 	void writeSecondaryResult() {
 		BenchmarkParams params = params("a=b");
-		Collection<BenchmarkResult> data = Arrays.<BenchmarkResult> asList(new BenchmarkResult(null, data("bar")));
+		Collection<BenchmarkResult> data = Collections.singletonList(new BenchmarkResult(null, data("bar")));
 		RunResult runResult = new RunResult(params, data);
 		String result = output(runResult);
-		// System.err.println(result);
 		assertThat(result).containsSubsequence("class, method, a, bar, median, mean, range", "Foo, exec, b");
 	}
 
 	@Test
 	void writeMixedParamResults() {
-		Collection<BenchmarkResult> data = Arrays.<BenchmarkResult> asList(new BenchmarkResult(null, data()));
+		Collection<BenchmarkResult> data = Collections.singletonList(new BenchmarkResult(null, data()));
 		String result = output(new RunResult(params("a=b"), data), new RunResult(params("a=e", "c=d"), data));
-		// System.err.println(result);
 		assertThat(result).containsSubsequence("class, method, a, c, median, mean, range", "Foo, exec, b, ,", "e, d");
 	}
 
 	@Test
 	void writeMixedSecondaryResults() {
-		Collection<BenchmarkResult> data = Arrays.<BenchmarkResult> asList(new BenchmarkResult(null, data()));
-		Collection<BenchmarkResult> seconds = Arrays
-				.<BenchmarkResult> asList(new BenchmarkResult(null, data("bar", "spam")));
+		Collection<BenchmarkResult> data = Collections.singletonList(new BenchmarkResult(null, data()));
+		Collection<BenchmarkResult> seconds = Collections.singletonList(new BenchmarkResult(null, data("bar", "spam")));
 		String result = output(new RunResult(params(), data), new RunResult(params(), seconds));
-		// System.err.println(result);
 		assertThat(result).containsSubsequence("class, method, bar, spam, median, mean, range", "Foo, exec, , ",
-				"Foo, exec, 1.000");
+				"Foo, exec, " + ScoreFormatter.format(1.000));
 	}
 
 	private BenchmarkParams params(String... workloads) {
@@ -132,26 +119,24 @@ public class CsvResultsWriterFactoryTests {
 				workload.put(key, value, i);
 			}
 		}
-		return new BenchmarkParams("com.example.Foo.exec", "bar", true, 1, new int[] { 1 }, Arrays.asList("thread"), 1, 0,
+		return new BenchmarkParams("com.example.Foo.exec", "bar", true, 1, new int[]{1}, Collections.singletonList("thread"), 1, 0,
 				null, null, Mode.AverageTime, workload, TimeUnit.MILLISECONDS, 1, "", Collections.emptyList(), "1.8", "JDK",
 				"1.8", "1.21", TimeValue.NONE);
 	}
 
-	private Collection<IterationResult> data(String... seconds) {
+	private static Collection<IterationResult> data(String... seconds) {
 		IterationResult result = new IterationResult(null, null, null);
 		result.addResult(new AverageTimeResult(ResultRole.PRIMARY, "foo", 15, 300000000, TimeUnit.MILLISECONDS));
 		for (String label : seconds) {
 			result.addResult(new AverageTimeResult(ResultRole.SECONDARY, label, 1, 1000000, TimeUnit.MILLISECONDS));
 		}
-		return Arrays.asList(result);
+		return Collections.singletonList(result);
 	}
 
 	private String output(RunResult... runResult) {
 		OutputStream stream = new ByteArrayOutputStream();
 		OutputFormat output = OutputFormatFactory.createFormatInstance(new PrintStream(stream), VerboseMode.NORMAL);
 		factory.forUri("csv:" + TARGET_FILE).write(output, Arrays.asList(runResult));
-		String result = stream.toString();
-		return result;
+		return stream.toString();
 	}
-
 }
